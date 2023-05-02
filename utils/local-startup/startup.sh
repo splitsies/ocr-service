1376972574e5de56eda0ef05648e7ec15340dfbf
Kevin Chen <kevchen21@gmail.com>
2023-05-01 17:00:49 -0700

    startup script

---
+++ utils/local-startup/startup.sh
@@ -0,0 +1,3 @@
+sls offline start \
+    --param='TESSERACT_WORKER_PATH=../../../../node_modules/tesseract.js/src/worker-script/node/index.js' \
+    --param='TESSERACT_CORE_PATH=../../../../node_modules/tesseract.js-core/tesseract-core.js'
