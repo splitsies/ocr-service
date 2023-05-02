2afc4a79c35595ae74fa2c23ac7508d29341daca
Kevin Chen <kevchen21@gmail.com>
2023-05-01 21:59:20 -0700

    allow absolute paths

--- utils/local-startup/startup.sh
+++ utils/local-startup/startup.sh
@@ -1,3 +1,1 @@
-sls offline start \
-    --param='TESSERACT_WORKER_PATH=../../../tesseract/dependencies/js/src/worker-script/node/index.js' \
-    --param='TESSERACT_CORE_PATH=../../../tesseract/dependencies/core/tesseract-core.js'
+sls offline start
