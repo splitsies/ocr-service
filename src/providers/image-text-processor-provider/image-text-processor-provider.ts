f695976a9cc7448b8bfd0c38a7457950230ad7a7
Kevin Chen <kevchen21@gmail.com>
2023-12-04 18:40:34 -0800

    add logging processor type

--- src/providers/image-text-processor-provider/image-text-processor-provider.ts
+++ src/providers/image-text-processor-provider/image-text-processor-provider.ts
@@ -4,10 +4,12 @@ import { ITesseractImageTextProcessor } from "@processors/tesseract-image-text-p
 import { ITextractImageTextProcessor } from "@processors/textract-image-text-processor/textract-image-text-processor-interface";
 import { inject, injectable } from "inversify";
 import { IImageTextProcessorProvider } from "./image-text-processor-provider-interface";
+import { ILogger } from "@splitsies/utils";
 
 @injectable()
 export class ImageTextProcessorProvider implements IImageTextProcessorProvider {
     constructor(
+        @inject(ILogger) private readonly _logger: ILogger,
         @inject(IOcrConfiguration) private readonly _ocrConfiguration: IOcrConfiguration,
         @inject(ITextractImageTextProcessor) private readonly _textractImageTextProcessor: ITextractImageTextProcessor,
         @inject(ITesseractImageTextProcessor)
@@ -17,8 +19,10 @@ export class ImageTextProcessorProvider implements IImageTextProcessorProvider {
     provide(): IImageTextProcessor {
         switch (this._ocrConfiguration.ocrEngine) {
             case "textract":
+                this._logger.log("Using textract processor");
                 return this._textractImageTextProcessor;
             default:
+                this._logger.log("Using tesseract processor");
                 return this._tesseractImageTextProcessor;
         }
     }
